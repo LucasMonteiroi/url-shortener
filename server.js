4ad721870cd3c2e861eb87e39007ddc9a5381958
Lucas Monteiro Inacio <lucas.inacio@restoque.com.br>
2020-08-17 16:53:01 -0300

    fix: port listener

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv/config');
 const app = require('./src/app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 // eslint-disable-next-line no-console
 app.listen(port, console.log(`Listening on port ${port}`));
